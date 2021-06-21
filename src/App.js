ff967d4e6a4d547ca38ab4103e192b0a5a3a2a19
izzilowe <izzilowe7@gmail.com>
2021-06-21 19:27:30 +0300

    styles

--- src/App.js
+++ src/App.js
@@ -3,54 +3,52 @@ import ToDo from "./ToDo";
 import ToDoForm from "./ToDoForm";
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   const addTask = (userInput) => {
-    if(userInput) {
+    if (userInput) {
       const newItem = {
-        id: Math.random().toString(36).substr(2,9),
+        id: Math.random().toString(36).substr(2, 9),
         task: userInput,
-        complete: false
-      }
-      setTodos([...todos, newItem])
+        complete: false,
+      };
+      setTodos([...todos, newItem]);
     }
-  }
+  };
 
   const removeTask = (id) => {
-    setTodos([...todos.filter((todo) => todo.id !== id)])
-  }
+    setTodos([...todos.filter((todo) => todo.id !== id)]);
+  };
 
   const handleToggle = (id) => {
     setTodos([
-      ...todos.map((todo) => 
-        todo.id === id ? {...todo, complete: !todo.complete } : {...todo}
-      )
-    ])
-  }
+      ...todos.map((todo) =>
+        todo.id === id ? { ...todo, complete: !todo.complete } : { ...todo }
+      ),
+    ]);
+  };
 
   return (
     <div className="App">
       <header>
-        <h1>Список задач: {todos.length}</h1> 
+        <h1>Список задач: {todos.length}</h1>
       </header>
-      <ToDoForm  addTask={addTask}/>
+      <ToDoForm addTask={addTask} />
       {todos.map((todo) => {
         return (
           <ToDo
-          todo={todo}
-          key={todo.id}
-          toggleTask={handleToggle}
-          removeTask={removeTask}
-           />
-        )
+            todo={todo}
+            key={todo.id}
+            toggleTask={handleToggle}
+            removeTask={removeTask}
+          />
+        );
       })}
     </div>
   );
 }
 
 export default App;
-
-
 
 // import { useState } from 'react'
 // import ToDo from './ToDo'
@@ -76,7 +74,7 @@ export default App;
 
 //   const handleToggle = (id) => {
 //     setTodos([
-//       ...todos.map((todo) => 
+//       ...todos.map((todo) =>
 //         todo.id === id ? { ...todo, complete: !todo.complete } : {...todo }
 //       )
 //     ])
--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -1,38 +1,36 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function ToDoForm({ addTask }) {
-    const [userInput, setUserInput] = useState("")
+  const [userInput, setUserInput] = useState("");
 
-    const handleChange = (event) => {
-        setUserInput(event.currentTarget.value)
+  const handleChange = (event) => {
+    setUserInput(event.currentTarget.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addTask(userInput);
+    setUserInput("");
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSubmit(event);
     }
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        addTask(userInput)
-        setUserInput("")
-    }
-
-    const handleKeyPress = (event) => {
-        if(event.key === "Enter") {
-            handleSubmit(event)
-        }
-    }
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <input
-                value={userInput}
-                type="text"
-                onChange={handleChange}
-                onKeyDown={handleKeyPress}
-                placeholder="Введите значение..."
-            />
-            <button>Сохранить</button> 
-        </form>
-    )
-    
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={userInput}
+        type="text"
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+        placeholder="Введите значение..."
+      />
+      <button>Сохранить</button>
+    </form>
+  );
 }
 
-export default ToDoForm
-
+export default ToDoForm;
